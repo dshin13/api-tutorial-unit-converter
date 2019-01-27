bed77b1f34100b9bd650f63981d3a1242b2ad060
Glitch (night-catfish) <none>
2019-01-27 15:36:57 +0000

    👨‍❤️‍💋‍👨✌️ Updated with Glitch

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+var expect = require('chai').expect;
+var ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  var convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+    .get(function (req, res){
+      var input = req.query.input;
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+    
+      // Invalid input filter
+      if (initNum == "invalid number") {
+        if (initUnit == "invalid unit") {
+          res.status(400).type('text').send('invalid number and unit')
+        }
+        else res.status(400).type('text').send('invalid number')
+      }
+      else if (initUnit == "invalid unit") {
+        res.status(400).type('text').send('invalid unit')
+      }    
+    
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+      
+      res.json({initNum, initUnit, returnNum, returnUnit, string: toString});
+    });
+    
+};
